09290d63df13dc3ae0907271ab4b200fb8d00b6a
Daniel Moreto <dfmoreto@gmail.com>
2017-06-05 02:37:31 -0300

    AdminLTE and its plugin was added as vendor assets

---
+++ vendor/assets/javascripts/admin_lte.js
@@ -0,0 +1,8 @@
+//= require admin_lte/bootstrap
+//= require admin_lte/fastclick
+//= require admin_lte/moment
+//= require admin_lte/fullcalendar
+//= require admin_lte/select2/select2.full
+//= require_directory ./admin_lte/select2/i18n
+//= require admin_lte/slimscroll
+//= require admin_lte/admin_lte
---
+++ vendor/assets/javascripts/select2.js
@@ -0,0 +1,2 @@
+//= require select2/select2.full
+//= require_directory ./select2/i18n
