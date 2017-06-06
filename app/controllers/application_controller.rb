c73904579cd72723b7f207f6ace1218748c5518c
Daniel Moreto <dfmoreto@gmail.com>
2017-06-05 22:25:17 -0300

    ApplicationController was updated to render right layout based on User login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  layout :define_layout
+
+
+  private
+
+
+  def define_layout
+    return "application" if user_signed_in?
+    return "login"
+  end
 end
