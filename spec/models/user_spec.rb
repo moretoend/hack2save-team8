c938b2b5b5ed1a1917d76f2f4135ad14cadd62d7
Daniel Moreto <dfmoreto@gmail.com>
2017-06-07 00:23:58 -0300

    User tests was updated

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it 'create a valid user' do
     user = create(:user)
-
     expect(user).to be_valid
   end
+
+  it { is_expected.to have_db_column :document_number }
+  it { is_expected.to have_db_column :fullname }
+  it { is_expected.to have_db_column :gender }
+  it { is_expected.to enumerize(:gender).in(:male, :female) }
+
 end
