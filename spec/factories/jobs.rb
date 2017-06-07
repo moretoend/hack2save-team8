99f206b89abb99fee777c6cd2e996de43a445101
Daniel Moreto <dfmoreto@gmail.com>
2017-06-07 20:48:18 -0300

    Spec for model Job

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :job do
     organization nil
-    title "MyString"
-    kind 1
-    places "MyString"
-    description "MyText"
+    title "Job 01"
+    kind "War"
+    places 2
+    description "Help families on Iraq war"
     start_date "2017-06-06"
-    end_date "2017-06-06"
+    end_date "2017-07-15"
   end
 end
