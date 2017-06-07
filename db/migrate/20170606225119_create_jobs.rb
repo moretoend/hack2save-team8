968cac6c6ec6e8cd926b338ccb9407d51c302187
Ricardo Silva <riksilvap@gmail.com>
2017-06-07 00:08:30 -0300

    Destroy model Organization and change fields Top kind:string, places:integer

--- db/migrate/20170606225119_create_jobs.rb
+++ db/migrate/20170606225119_create_jobs.rb
@@ -3,8 +3,8 @@ class CreateJobs < ActiveRecord::Migration[5.1]
     create_table :jobs do |t|
       t.references :organization, foreign_key: true
       t.string :title
-      t.integer :kind
-      t.string :places
+      t.string :kind
+      t.integer :places
       t.text :description
       t.date :start_date
       t.date :end_date
--- spec/factories/organizations.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :organization do
-    name "MyString"
-    kind_of_services 1
-  end
-end
