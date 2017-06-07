330a2a0589f34896fafaf49b94abf76fc0ce7cbf
Daniel Moreto <dfmoreto@gmail.com>
2017-06-07 19:08:35 -0300

    A params permit override of Devise for account_update was added on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < ActionController::Base
 
   layout :define_layout
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected
+
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:email, :password, :current_password, :document_number, :fullname, :gender)
+      end
+  end
+
 
   private
 
