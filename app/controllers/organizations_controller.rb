e0ecdb77b47806e05e6a9d87a431e504e05de410
Ricardo Eihara <ricardo.hejr@gmail.com>
2017-06-07 02:02:27 -0300

    Refactor and match organization views with layout theme
    - Add user/organization association
    - Add bootstrap-sass gem
    - DRY organizations_controller
    - Improve organizations views with admin_lte
    - Add _form to organizations
    - Add simple_form gem
    - Add 'about' column on organization model

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,5 @@
 class OrganizationsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  before_action :find_organization_by_id, only: [:update, :edit, :show]
 
   def index
     @organizations = Organization.all
@@ -16,31 +16,30 @@ class OrganizationsController < ApplicationController
       @organization.save
       redirect_to @organization
     else
-      flash.now[:error] = 'aaaaaa'
+      flash.now[:error] = 'Could not create organization'
       render :new
     end
   end
 
-  def show
-    @organization = Organization.find_by(id: params[:id])
-  end
+  def show; end
 
-  def edit
-    @organization = Organization.find_by(id: params[:id])
-  end
+  def edit; end
 
   def update
-    @organization = Organization.find_by(id: params[:id])
     if @organization.update(organization_params)
       redirect_to @organization
     else
-      flash.now[:error] = 'Não foi possível editar'
+      flash.now[:error] = 'Could not edit organization'
       render :edit
     end
   end
 
   private
     def organization_params
-      params.require(:organization).permit(:name, :cnpj, :phone, :email)
+      params.require(:organization).permit(:name, :cnpj, :phone, :email, :service_kind, :about)
+    end
+
+    def find_organization_by_id
+      @organization = Organization.find_by(id: params[:id])
     end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,6 @@
 class Organization < ApplicationRecord
+  belongs_to :responsible, class_name: :User, foreign_key: :user_id
+
+  enum service_kind: [ :industry, :commerce, :service ]
   validates :name, :cnpj, :phone, presence: true
 end
