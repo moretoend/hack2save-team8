bcbc21481a126ab8b6f868095e179df82037b713
Daniel Moreto <dfmoreto@gmail.com>
2017-06-08 22:37:44 -0300

    Fixed belongs_to association with :responsible on model Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,5 @@
 class Organization < ApplicationRecord
-  belongs_to :responsible, class_name: :User, foreign_key: :user_id
+  belongs_to :responsible, class_name: User, foreign_key: "user_id"
   has_many :jobs
 
   enum service_kind: [ :industry, :commerce, :service ]
