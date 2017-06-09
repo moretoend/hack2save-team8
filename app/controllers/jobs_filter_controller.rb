a524369cc8099f5b0fb733fe61dc92d1511c610a
Daniel Moreto <dfmoreto@gmail.com>
2017-06-09 01:25:32 -0300

    JobsFilterController and its view files were created

---
+++ app/controllers/jobs_filter_controller.rb
@@ -0,0 +1,13 @@
+class JobsFilterController < ApplicationController
+  def index
+    @jobs = load_jobs.where("title LIKE :search OR kind LIKE :search", search: "%#{params[:search]}%")
+  end
+
+
+  private
+
+
+  def load_jobs
+    Job.all.where.not(id: current_user.jobs.ids)
+  end
+end
---
+++ spec/controllers/filter_jobs_controller_spec.rb
@@ -0,0 +1,47 @@
+describe JobsFilterController, type: :controller do
+
+  let(:user) { create(:user) }
+
+  before { sign_in user }
+
+
+  context "GET #index" do
+    let(:user_organization) { create(:organization, responsible: user) }
+    let(:no_user_organization) { create(:organization) }
+
+    let!(:user_organization_jobs) { create_list(:job, 5, organization: user_organization) }
+    let!(:no_user_organization_jobs) { create_list(:job, 5, organization: no_user_organization) }
+
+    context "without :search text" do
+      it "returns all @jobs out of his organization" do
+        get :index
+        expect(assigns(:jobs)).to eq no_user_organization_jobs
+      end
+
+      it "must not return @jobs of his organization" do
+        get :index
+        expect(assigns(:jobs)).to_not match_array(user_organization_jobs)
+      end
+    end
+
+    context "with :search text" do
+      let!(:no_user_organization_search_jobs) { create_list(:job, 5, title: "Search", organization: no_user_organization) }
+      let!(:user_organization_search_jobs) { create_list(:job, 5, title: "Search", organization: user_organization) }
+
+      it "returns all @jobs out of his organization that following search text" do
+        get :index, params: { search: "Search" }
+        expect(assigns(:jobs)).to eq no_user_organization_search_jobs
+      end
+
+      it "must not returns @jobs of his organization that following search text" do
+        get :index, params: { search: "Search" }
+        expect(assigns(:jobs)).to_not match_array(user_organization_search_jobs)
+      end
+
+      it "must not returns @jobs that doesn't follow search text" do
+        get :index, params: { search: "Search" }
+        expect(assigns(:jobs)).to_not include(user_organization_jobs + no_user_organization_jobs)
+      end
+    end
+  end
+end
