0385b1428137c172c19da2efb1b20e52615379bf
Daniel Moreto <dfmoreto@gmail.com>
2017-06-09 23:08:24 -0300

    Added rating starts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require rating
 //= require datepicker
 //= require admin_lte
 //= require rails-ujs
@@ -21,5 +22,12 @@
 $(document).ready(function(){
   $(".act_as_datepicker").datepicker({
     format: "dd/mm/yyyy"
+  });
+
+  $(".rating").rating({
+    start: 0,
+    stop: 5,
+    empty: 'fa fa-icon fa-start-o',
+    filled: 'fa fa-icon fa-start'
   });
 });
